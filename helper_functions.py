a14f2708c9724d66aa4241a1b28cb811f62f9775
Erlend Flatland <99878253+eflatlan@users.noreply.github.com>
2023-05-23 17:02:52 +0200

    surrekopp:)

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
+from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import norm
-from __future__ import print_function
 import os
 import h5py
 import tensorflow as tf
