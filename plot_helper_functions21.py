ddc9bbf21f19341ba626438a196c54b6a1b1d977
Erlend Flatland <99878253+eflatlan@users.noreply.github.com>
2023-09-24 21:31:26 +0000

    Update and rename plot_helper_functions20.py to plot_helper_functions21.py

--- plot_helper_functions21.py
+++ plot_helper_functions21.py
@@ -1,105 +1,91 @@
 from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import label_binarize
-from sklearn.metrics import precision_recall_curve, confusion_matrix
-print_function
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import precision_recall_curve, confusion_matrix
 
-def plot_confusion_matrix(ax, cm):
+def plot_confusion_matrix(ax, cm, title="Confusion Matrix"):
     """Utility function to plot the confusion matrix."""
     ax.imshow(cm, cmap='Blues', interpolation='nearest')
     ax.set_xticks(np.arange(3))
     ax.set_yticks(np.arange(3))
     ax.set_xticklabels(['0', '1', '2'])
     ax.set_yticklabels(['0', '1', '2'])
+    ax.set_title(title)
     for x in range(3):
         for y in range(3):
             percent = cm[x, y] / np.sum(cm[x, :]) * 100  # Percentage formula
             ax.text(y, x, f"{cm[x, y]} ({percent:.1f}%)", ha='center', va='center', color='red')
 
 def plot_training_history(history, y_pred_train, y_pred_test, y_train_true, y_test_true):
+    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
 
-    fig, axs = plt.subplots(4, 4, figsize=(25, 20))
-    
-    # Parameters for annotation
-    params = []
-
-    annotation = '\n'.join(params)
-
-    # Add annotation to each axis
-    for row_axes in axs:
-        for ax in row_axes:
-            ax.annotate(annotation, (0.05, 0.95), textcoords='axes fraction',
-                        bbox=dict(boxstyle="square", fc="white", ec="black"),
-                        va="top", ha="left")
-    
-    # Plots for Overall data
-    labels = ['Overall Loss', 'Overall Accuracy', 'Overall P-R Curve', 'Overall Confusion Matrix']
-
-    # Loss & Accuracy
+    # Train Loss & Accuracy
     axs[0, 0].plot(history.history['loss'], label="Train")
-    axs[0, 0].plot(history.history['val_loss'], label="Validation")
+    axs[0, 0].set_title('Training Loss')
     axs[0, 0].legend()
-    axs[0, 0].set_title(labels[0])
 
     axs[0, 1].plot(history.history['accuracy'], label="Train")
-    axs[0, 1].plot(history.history['val_accuracy'], label="Validation")
+    axs[0, 1].set_title('Training Accuracy')
     axs[0, 1].legend()
-    axs[0, 1].set_title(labels[1])
+
+    # Validation Loss & Accuracy
+    axs[1, 0].plot(history.history['val_loss'], label="Validation")
+    axs[1, 0].set_title('Validation Loss')
+    axs[1, 0].legend()
+
+    axs[1, 1].plot(history.history['val_accuracy'], label="Validation")
+    axs[1, 1].set_title('Validation Accuracy')
+    axs[1, 1].legend()
+
+    # P-R Curve & Confusion Matrix for both train and validation data
+    fig, axs = plt.subplots(1, 4, figsize=(25, 6))
     
-    # P-R Curve for overall data
+    # P-R Curve for train data
     y_train_bin = label_binarize(y_train_true, classes=[0, 1, 2])
-    y_test_bin = label_binarize(y_test_true, classes=[0, 1, 2])
     for i in range(3):
         precision_train, recall_train, _ = precision_recall_curve(y_train_bin[:, i], y_pred_train[:, i])
-        axs[0, 2].plot(recall_train, precision_train, lw=2, label=f"Class {i}")
-        axs[0, 2].set_title(labels[2])
-        axs[0, 2].legend()
+        axs[0].plot(recall_train, precision_train, lw=2, label=f"Class {i}")
+    axs[0].set_title("Train P-R Curve")
+    axs[0].legend()
+
+    # P-R Curve for validation data
+    y_test_bin = label_binarize(y_test_true, classes=[0, 1, 2])
+    for i in range(3):
+        precision_test, recall_test, _ = precision_recall_curve(y_test_bin[:, i], y_pred_test[:, i])
+        axs[1].plot(recall_test, precision_test, lw=2, label=f"Class {i}")
+    axs[1].set_title("Validation P-R Curve")
+    axs[1].legend()
+
+    # Confusion Matrix for train data
+    cm_train = confusion_matrix(y_train_true, y_pred_train)
+    plot_confusion_matrix(axs[2], cm_train, title="Train Confusion Matrix")
     
-    # Confusion Matrix
-    cm = confusion_matrix(y_train_true.argmax(axis=1), y_pred_train.argmax(axis=1))
-    axs[0, 3].set_title(labels[3])
-    plot_confusion_matrix(axs[0, 3], cm)
-
-    # Plots for each class separately
-    for idx in range(3):
-        axs[1, idx].plot(history.history['loss'], label="Train")
-        axs[1, idx].plot(history.history['val_loss'], label="Validation")
-        axs[1, idx].legend()
-        axs[1, idx].set_title(f"Species {idx+1} - Loss")
-
-        axs[2, idx].plot(history.history['accuracy'], label="Train")
-        axs[2, idx].plot(history.history['val_accuracy'], label="Validation")
-        axs[2, idx].legend()
-        axs[2, idx].set_title(f"Species {idx+1} - Accuracy")
-
-        # P-R curve for each species
-        precision_train, recall_train, _ = precision_recall_curve(y_train_bin[:, idx], y_pred_train[:, idx])
-        axs[3, idx].plot(recall_train, precision_train, lw=2)
-        axs[3, idx].set_title(f"Species {idx+1} - Precision vs Recall")
+    # Confusion Matrix for validation data
+    cm_test = confusion_matrix(y_test_true, y_pred_test)
+    plot_confusion_matrix(axs[3], cm_test, title="Validation Confusion Matrix")
 
     plt.tight_layout()
     plt.show()
+
+
+def freedman_diaconis_bins(data):
+    """Calculate number of hist bins using Freedman-Diaconis rule."""
+    # Assuming data is 1D array
+    data_range = np.nanmax(data) - np.nanmin(data)
+    iqr = np.subtract(*np.percentile(data, [75, 25]))
+    bin_width = 2 * iqr * (len(data) ** -0.33)
+    return int(data_range / bin_width) if bin_width > 0 else 1
+
+def check_nan_inf(arr):
+    """Check if array contains NaN or Inf values."""
+    return np.isnan(arr).any(), np.isinf(arr).any()
+
 def plot_hist(X_train=None, X_test=None, description=None):
-    def freedman_diaconis_bins(data):
-        data = np.asarray(data)
-        data_range = np.nanmax(data) - np.nanmin(data)
-        iqr = np.subtract(*np.percentile(data, [75, 25]))
-        bin_width = 2 * iqr * (len(data) ** -0.33)
-        return int(data_range / bin_width) if bin_width > 0 else 1
-
-    def check_nan_inf(arr):
-        return np.isnan(arr).any(), np.isinf(arr).any()
-
-    for label, variable in {**X_train, **X_test}.items():
-        has_nan, has_inf = check_nan_inf(variable)
-        if has_nan or has_inf:
-            warnings.warn(f"{label} contains NaN or Inf values. This may lead to issues.")
-
     fig, axs = plt.subplots(2, 5, figsize=(25, 10))
     fig.suptitle(f"Training and Testing Data: {description}", fontsize=20)
     fig1, axs1 = plt.subplots(2, 3, figsize=(18, 10))
@@ -110,9 +96,13 @@ def plot_hist(X_train=None, X_test=None, description=None):
     def plot_routine(variables, row_idx):
         axs_idx = 0
         for label, variable in variables.items():
+            has_nan, has_inf = check_nan_inf(variable)
+            if has_nan or has_inf:
+                warnings.warn(f"{label} contains NaN or Inf values. This may lead to issues.")
+
             bins = freedman_diaconis_bins(variable)
             range_val = (np.nanmin(variable), np.nanmax(variable))
-            
+
             if label == 'MIP Position' or label == 'Rad Position':
                 variable = np.asarray(variable).reshape(-1, 2)
                 mask = (variable[:, 0] != 0) & (variable[:, 1] != 0)
@@ -123,33 +113,28 @@ def plot_hist(X_train=None, X_test=None, description=None):
                 axs1[row_idx, 1].set_title(f"{label} X")
                 axs1[row_idx, 2].hist(variable[:, 1], bins=bins, range=range_val, edgecolor='black')
                 axs1[row_idx, 2].set_title(f"{label} Y")
+
             elif label == 'Pion Candidates':
                 variable = np.asarray(variable)
                 mask = np.any(variable != 0, axis=-1)
                 filtered_variable = variable[mask]
-                variable = np.asarray(variable)
-                ranges = [(0,125),(0,125),(0,11),(0,200)]
-                bins = [10, 0, 1, 20]
+                ranges = [(0, 125), (0, 125), (0, 11), (0, 300)]
+                bins_values = [25, 25, 12, 30]
+                titles = ['Pion X', 'Pion Y', 'Pion Size', 'Pion Charge']
                 for i in range(4):
-
-                    if i = 3: 
-                        range = (0, 11)
-                        bins = 12
-                    elif i == 4: range = (0,300)
-                        bins = 30
-                    else: range = (0, 125)
-                        bins = 25
-                    axs2[row_idx, i].hist(variable[:, :, i][variable[:, :, i] > 0], bins=bins, range=range, edgecolor='black')
-                    titles = ['Pion X', 'Pion Y', 'Pion Size', 'Pion Charge']
+                    axs2[row_idx, i].hist(variable[:, i][mask], bins=bins_values[i], range=ranges[i], edgecolor='black')
                     axs2[row_idx, i].set_title(f"{'Train' if row_idx == 0 else 'Test'} {titles[i]}")
-                    
+
             elif label == 'mCluSize':
                 axs[row_idx, axs_idx].hist(variable, bins=13, range=(0, 12), edgecolor='black')
                 axs[row_idx, axs_idx].set_title(f"{'Train' if row_idx == 0 else 'Test'} {label}")
+                axs_idx += 1
 
             elif label == 'Momentum':
                 axs[row_idx, axs_idx].hist(variable, bins=50, range=(0, 5), edgecolor='black')
                 axs[row_idx, axs_idx].set_title(f"{'Train' if row_idx == 0 else 'Test'} {label}")
+                axs_idx += 1
+
             else:
                 axs[row_idx, axs_idx].hist(variable, bins=bins, range=range_val, edgecolor='black')
                 axs[row_idx, axs_idx].set_title(f"{'Train' if row_idx == 0 else 'Test'} {label}")
