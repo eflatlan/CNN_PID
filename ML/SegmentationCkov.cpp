41ef77a9626d69062247816a7d35af91e5ab88a4
Erlend Flatland <erlend.flatland@cern.ch>
2023-10-04 01:46:32 +0200

    added segment on ckov single photon recon

--- ML/SegmentationCkov.cpp
+++ ML/SegmentationCkov.cpp
@@ -9,7 +9,7 @@
 #include "SimulationDataFormat/MCCompLabel.h"
 
 #include "HmpidDataReader.cpp"
-#include "CkovTools.cpp"
+#include "CkovToolsSingle.cpp"
 //#include "CkovToolsErlend.cpp"
 #include "ParticleUtils2.cpp"
 
