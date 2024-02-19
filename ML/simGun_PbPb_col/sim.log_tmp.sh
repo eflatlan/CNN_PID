48c3754252e69770f94bcb148b6da2d2ded013b4
eflatlan <erlend.flatland@cern.ch>
2024-02-19 13:36:04 +0100

    o2match_hmp.rot is not writeable

--- ML/simGun_PbPb_col/sim.log_tmp.sh
+++ ML/simGun_PbPb_col/sim.log_tmp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export LIBC_FATAL_STDERR_=1
-o2-grp-simgrp-tool createGRPs --run 300000 --publishto GRP -o mcGRP;
+o2-sim -n1 --configKeyValues Diamond.width[2]=6. -g pythia8hi -e TGeant3 -j 5 --run 300000;
 RC=$?; echo "TASK-EXIT-CODE: ${RC}"; exit ${RC}
